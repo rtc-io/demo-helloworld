ec3428cfb0300be1dc12e49f623fb04d4f91ec7f
Damon Oehlman <damon.oehlman@gmail.com>
2014-10-14 10:18:31 +1100

    Created process for copying approriate vendor modules

--- gulpfile.js
+++ gulpfile.js
@@ -4,5 +4,16 @@ gulp.task('default', function() {
   // place code for your default task here
 });
 
-gulp.task('vendor', function() {
+gulp.task('vendor', ['vendor-rtc', 'vendor-rtc-ios']);
+
+gulp.task('vendor-rtc', function() {
+  return gulp
+    .src('node_modules/rtc/dist/*')
+    .pipe(gulp.dest('vendor/'));
+});
+
+gulp.task('vendor-rtc-ios', function() {
+  return gulp
+    .src('node_modules/rtc-plugin-nicta-ios/dist/*')
+    .pipe(gulp.dest('vendor/'));
 });
