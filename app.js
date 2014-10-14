8a19394ab27d3e75b0882dd16a1a4fe32f05a219
Damon Oehlman <damon.oehlman@gmail.com>
2014-10-14 14:47:29 +1100

    Added google stun servers

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-RTC({ plugins: [ RTC.IOS ]});
+RTC({
+  // use the public google stun servers :)
+  ice: [
+    { url: 'stun1.l.google.com:19302' },
+    { url: 'stun2.l.google.com:19302' },
+    { url: 'stun3.l.google.com:19302' },
+    { url: 'stun4.l.google.com:19302' }
+  ],
+
+  // we want this to work on iOS as well so we will use
+  // the rtc-plugin-nicta-ios plugin so we can use the
+  // build.rtc.io to create a native iOS app
+  plugins: [ RTC.IOS ]
+});
