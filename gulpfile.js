d8df94fb77c4bada2734e8a8c4015e62a741a50d
Damon Oehlman <damon.oehlman@gmail.com>
2014-10-14 12:04:35 +1100

    Add archive creation to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
+var zip = require('gulp-zip');
 
-gulp.task('default', function() {
+gulp.task('default', ['package'], function() {
   // place code for your default task here
+});
+
+gulp.task('package', function() {
+  return gulp.src(['./*', 'vendor/*', '!archive.zip', '!gulpfile.js', '!package.json'], { base: '.' })
+    .pipe(zip('archive.zip'))
+    .pipe(gulp.dest('.//'));
 });
 
 gulp.task('vendor', ['vendor-rtc', 'vendor-rtc-ios']);
